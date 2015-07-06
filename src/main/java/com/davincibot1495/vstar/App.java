b43e8505f9eab86dc9fc2a029fb167d021972426
daVinciBot1495 <chazz184@gmail.com>
2015-07-05 20:02:10 -0700

    I've made a huge mistake.

---
+++ src/main/java/com/davincibot1495/vstar/App.java
@@ -0,0 +1,7 @@
+package com.davincibot1495.vstar;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("I've made a huge mistake.");
+	}
+}
---
+++ src/test/java/com/davincibot1495/vstar/AppTest.java
@@ -0,0 +1,11 @@
+package com.davincibot1495.vstar;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
